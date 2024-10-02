8e16791ee47478ef149dd783cc8215f318b5d01d
Ramy <ramy.bouzar@hotmail.fr>
2024-10-02 15:38:49 +0200

    ajout des fichiers

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+const fetch = require('node-fetch');
+var router = express.Router();
+
+module.exports = router;
+
+
+const url =`${process.env.Url_movie}api_key=${process.env.Api_key}`;
+console.log(url);
+// const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+
+
+router.get("/movies", (req, res) => {
+    // User.find().then((allUsers) => res.json({ result: true, allUsers }))
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+
+        const tabmovie = data.results.map((movieElem, i) =>{
+            console.log("une movie")
+            return { title: movieElem.title, 
+                poster: `https://image.tmdb.org/t/p/original/${movieElem.poster_path}`, 
+                voteAverage: movieElem.vote_average, 
+                voteCount: movieElem.vote_count, 
+                overview: movieElem.overview
+            }
+        })
+
+  res.json({tabmovie})
+})
+  .catch(err => console.error('error:' + err));
+
+})
+ // const movies = femovies.map((data, i) => {
+  //   const isLiked = likedMovies.some(movie => movie === data.title);
+  //   return <Movie key={i} updateLikedMovies={updateLikedMovies} isLiked={isLiked} title={data.title} overview={
+  //     // if(data.overview.length > 250) {
+  //     //   data.overview = data.overview.substring(0, 250) + '...';
+  //     }} poster={`https://image.tmdb.org/t/p/original/`+data.poster_path} voteAverage={data.vote_average} voteCount={data.vote_count} />;
+  // });
+
+//   if(data.overview.length > 250) {
+//       data.overview = data.overview.substring(0, 250) + '...';
